580335c7fea2cc76aa8c6b89c8c96c73c0d105ad
aizawaatsushi <atsushi.aizawa@rakus-partners.co.jp>
2019-08-01 16:22:11 +0900

    名前を表示させるHTML完成

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,19 +1,29 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.Exam01Form;
 
 @Controller
 @RequestMapping("/exam01")
 public class Exam01Controller {
 
+	@ModelAttribute
+	public Exam01Form setUpForm() {
+		return new Exam01Form();
+	}
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
 	}
 	
 	@RequestMapping("/exam01-result")
-	public String Exam01Result() {
+	public String Exam01Result(String name,Model model) {
+		model.addAttribute("name", name);
 		return "exam01-result";
 	}
 }
